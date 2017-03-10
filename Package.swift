a946411cebf5f72719212482caa05ed1830f5357
sonson <yoshida.yuichi@gmail.com>
2017-03-10 11:03:02 +0900

    Roll back to modulemap.

--- Package.swift
+++
@@ -1,9 +0,0 @@
-import PackageDescription
-
-let package = Package(
-    name: "CommonCrypto",
-    targets: [
-        Target(name: "CommonCrypto", dependencies: ["CCommonCrypto"]),
-        Target(name: "CCommonCrypto")
-    ]
-)
--- Tests/LinuxMain.swift
+++
@@ -1,6 +0,0 @@
-import XCTest
-@testable import CommonCryptoTests
-
-XCTMain([
-     testCase(CommonCryptoTests.allTests),
-])
